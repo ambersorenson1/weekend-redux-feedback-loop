b5ce7214452015321c336750a74ca38dd27437aa
ambersorenson1 <ambersorenson1@gmail.com>
2021-12-04 20:16:57 -0600

    imported the necessities for the HOME.js

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import { useHistory } from "react-router-dom";
+
+
+
+function Home(){
+  const history = useHistory();
+
+  const [feedback, setFeedback] = useState ([]);
+
+
+  useEffect (() => {
+
+  }, []);
+}
