8a792dfb54a1b4e5df624a0763d0ae091b2086c8
ambersorenson1 <ambersorenson1@gmail.com>
2021-12-04 19:22:49 -0600

    updated app.js with routes and the imports

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,36 +1,69 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
+
+//importing the components I will need to access 
+import Comments from '../Comments/Comments';
+import Feelings from '..Feelings/Feelings';
+import Home from '../Home/Home';
+import Support from '../Supported/Supported';
+import Understanding from '../Understanding/Understanding';
+
+
 
 function App() {
 
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
-  );
-}
-
-
-function refreshPizzas() {
+const  getFeedback = () => {
   axios({
     method: "GET",
-    url: "/api/pizza",
+    url: "/api/feedback",
   })
     .then((response) => {
-      console.log("Pizza Get response:", response.data);
+      console.log("Feedback Get response:", response.data);
       dispatch({
-        type: "SET_LIST",
+        type: "SET_FEEDBACK",
         payload: response.data,
       });
     })
     .catch((error) => {
-      console.log("/api/pizza GET error:", error);
+      console.log("/api/feedback GET error:", error);
     });
 };
 
+
+return (
+  <div className='App'>
+    <header className='App-header'>
+      <h1 className='App-title'>Feedback!</h1>
+      <h4>Don't forget it!</h4>
+    </header>
+    <Router>
+      <Route exact path ="/">
+        {/* <Home /> */}
+      </Route>
+      <Route exact path ="/">
+        {/* <Feeling /> */}
+      </Route>
+      <Route exact path ="/">
+        {/* <Understanding /> */}
+      </Route>
+      <Route exact path ="/">
+        {/* <Supported /> */}
+      </Route>
+      <Route exact path ="/">
+        {/* <Comments /> */}
+      </Route>
+      <Route exact path ="/">
+        {/* <Review /> */}
+      </Route>
+      <Route exact path ="/">
+        {/* <Submit /> */}
+      </Route>
+    </Router>
+  </div>
+);
+}
 
 export default App;
